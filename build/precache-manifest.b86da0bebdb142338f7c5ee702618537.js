81dd704c8b3065c5d96263c5ad5ea238ce7a68ad
kirtiman <kirtiman.patowary@knackbout.com>
2022-07-26 12:22:20 +0530

    Build made

---
+++ build/precache-manifest.b86da0bebdb142338f7c5ee702618537.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb030af891f1479955957dc8d7e1c17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebdb678748a20b345740",
+    "url": "/static/css/2.3e8e9077.chunk.css"
+  },
+  {
+    "revision": "b7b6e3af99257feffd7b",
+    "url": "/static/css/main.f131c187.chunk.css"
+  },
+  {
+    "revision": "ebdb678748a20b345740",
+    "url": "/static/js/2.46ae85c4.chunk.js"
+  },
+  {
+    "revision": "665bab5f39d3877a9fbe4563fc2aa99e",
+    "url": "/static/js/2.46ae85c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b6e3af99257feffd7b",
+    "url": "/static/js/main.21ac96f1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1e4d4f8bfd491f6df0765d92dd91b93d",
+    "url": "/static/media/Aviation-airport-Boeing-737-aircraft_2560x1600.1e4d4f8b.jpg"
+  },
+  {
+    "revision": "6a448f0cb8b6bde22018fbfe73b806ff",
+    "url": "/static/media/Logo-Horizonal-08.6a448f0c.png"
+  },
+  {
+    "revision": "ec8d20fe5e41d6e25162b6fb688d9863",
+    "url": "/static/media/Under maintenance.ec8d20fe.png"
+  },
+  {
+    "revision": "c0af34953740c88f0bc582e3efeebc7e",
+    "url": "/static/media/arrow_up_green.c0af3495.png"
+  },
+  {
+    "revision": "54b8b93ddf90199a36a52967f690a36c",
+    "url": "/static/media/download.54b8b93d.svg"
+  },
+  {
+    "revision": "7b3350110288186f75d500de8b4153d4",
+    "url": "/static/media/ellipsis-loader.7b335011.svg"
+  },
+  {
+    "revision": "2aef78bdb87a1e231231e11e509d9b15",
+    "url": "/static/media/loader.2aef78bd.png"
+  }
+]);
